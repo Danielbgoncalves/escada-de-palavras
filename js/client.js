beab4dcb24b67ce7916058a362fe6ca18ef05d29
Daniel Borges <44danbor@gmail.com>
2026-05-20 22:16:58 -0300

    feature: implement sendMetricsToDB function for tracking game metrics

--- js/client.js
+++ js/client.js
@@ -3,3 +3,30 @@ export async function getChallengeFromServer(){
     const challenge = await response.json()
     return challenge
 }
+
+export async function sendMetricsToDB(score, gameState){
+    const dadosPartida = { //attempts, restarts, spentTime, challengeID
+        score: score,
+        attempts: gameState.attempts,
+        restarts: gameState.restarts,
+        spentTime: gameState.spentTime,
+        challengeID: gameState.challengeID
+    }
+
+    try {
+        const response = await fetch('https://escada-de-palavras-backend.onrender.com/api/enviar-jogo', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(dadosPartida)
+        })
+
+        const resultado = await response.json()
+        
+        if(resultado.sucesso){
+            console.log(resultado.mensagem)
+        }
+
+    } catch(error){
+        console.error('Falha ao enviar métricas', error)
+    }
+}
